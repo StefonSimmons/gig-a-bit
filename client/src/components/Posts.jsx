faf556f2f18b28aa4d9cf8a861ee55374713350b
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-11 12:34:54 -0400

    posts on page

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const PostSection = styled.div`
+  margin: 15px;
+  width: 306px;
+  // padding: 20px;
+  border: grey solid 3px;
+`
+const Image = styled.img`
+  width: 300px;
+`
+
+const Main = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 50px
+`
 
 export default function Posts({ posts }) {
-  
+
   const allPosts = posts.map((post, id) => {
     return (
       <React.Fragment key={id}>
-        <div>
-          <img src={post.media_link} alt="se(no)" />
+        <PostSection>
+          <Image src={post.media_link} alt="se(no)" />
           <h3>{post.user_id}</h3>
           <h4>{post.topic_id}</h4>
           <button>email here</button>
@@ -15,14 +32,16 @@ export default function Posts({ posts }) {
             <li>{post.bullet_two}</li>
             <li>{post.bullet_three}</li>
           </ul>
-        </div>
+        </PostSection>
       </React.Fragment>
     )
   })
 
   return (
-    <div>
-      {allPosts}
-    </div>
+    <>
+      <Main>
+        {allPosts}
+      </Main>
+    </>
   )
 }
