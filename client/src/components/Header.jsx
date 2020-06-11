96efb8c74ba9a1de804f161bf5494591d8e1933e
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-11 11:03:11 -0400

    added header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Head = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 45px
+`
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-family: 'Pathway Gothic One', sans-serif;  
+  font-weight: 700;
+  font-style: italic;
+  color: rgb(154, 78, 80)
+`
+const NavBar = styled.nav`
+`
+const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+`
+const NavItems = styled.li`
+  padding-right: 50px;
+  font-family: 'Pathway Gothic One', sans-serif;  
+  font-size: 20px
+`
+const Divider = styled.hr`
+  height: 2px;
+  background: grey;
+  margin: 0px
+`
 
 export default function Header() {
   return (
-    <div>
-      <header>
-        <h1>Gig-A-Bit</h1>
-        <nav>
-          <ul>
-            <li class="w3-xlarge"><i class="material-icons w3-xlarge">home</i></li>
-            <li>Create A Profile</li>
-            <li>Log In</li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <>
+      <Head>
+        <Title>Gig-A-Bit</Title>
+        <NavBar>
+          <NavList>
+            <NavItems><i class="material-icons w3-xxlarge">home</i></NavItems>
+            <NavItems>Create A Profile</NavItems>
+            <NavItems>Log In</NavItems>
+          </NavList>
+        </NavBar>
+      </Head>
+      <Divider/>
+    </>
   )
 }
