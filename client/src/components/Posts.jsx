f4a42832f2725dbad8010ca4058d9a69bb43b8c6
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-11 12:23:55 -0400

    mapped out posts

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
-export default function Posts({posts}) {
+export default function Posts({ posts }) {
+  
+  const allPosts = posts.map((post, id) => {
+    return (
+      <React.Fragment key={id}>
+        <div>
+          <img src={post.media_link} alt="se(no)" />
+          <h3>{post.user_id}</h3>
+          <h4>{post.topic_id}</h4>
+          <button>email here</button>
+          <ul>
+            <li>{post.bullet_one}</li>
+            <li>{post.bullet_two}</li>
+            <li>{post.bullet_three}</li>
+          </ul>
+        </div>
+      </React.Fragment>
+    )
+  })
+
   return (
     <div>
-      {console.log(posts)}
+      {allPosts}
     </div>
   )
 }
