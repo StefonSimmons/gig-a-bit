ca3f003bb1998dca0e819a73e71b059ef5f00791
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-10 23:04:16 -0400

    added header,footer and main components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import Main from './components/Main'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div> 
-      Hello
+    <div>
+      <Header />
+      <Main />
+      Hello From App
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+
+
+  
+  render() {
+    return (
+      <div>
+        Main Component
+      </div>
+    )
+  }
+}
