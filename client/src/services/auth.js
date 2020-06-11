b64c6b89c226a3af78fff9a428c8a140b2e5c746
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-10 22:21:51 -0400

    added auth functions

---
+++ client/src/services/auth.js
@@ -0,0 +1,33 @@
+import api from './apiConfig';
+
+export const loginUser = async (loginParams) => {
+  const resp = await api.post('/auth/login', { auth: loginParams })
+  const token = resp.data.token
+  const user = resp.data.user
+  localStorage.setItem('authToken', token);
+  api.defaults.headers.common.authorization = `Bearer ${token}`
+  return user
+}
+
+export const registerUser = async (registerParams) => {
+  const resp = await api.post('/users', { user: registerParams })
+  const token = resp.data.token
+  const user = resp.data.user
+  localStorage.setItem('authToken', token);
+  api.defaults.headers.common.authorization = `Bearer ${token}`
+  return user
+}
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`
+    const resp = await api.get('/auth/verify');
+    return resp.data
+  }
+  return false
+}
+
+export const removeToken = () => {
+  api.defaults.headers.common.authorization = null;
+}
