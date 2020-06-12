726332a541cff71b0fb8f5dee2bc67762df0f7b6
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-12 09:58:12 -0400

    auth login working

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,10 +1,8 @@
 import api from './apiConfig';
 
 export const loginUser = async (loginParams) => {
-  console.log(loginParams)
-  const resp = await api.get('/auth/login', { auth: loginParams })
-  console.log(loginParams)
-  // console.log(resp)
+
+  const resp = await api.post('/auth/login', { auth: loginParams })
   const token = resp.data.token
   const user = resp.data.user
   localStorage.setItem('authToken', token);
@@ -13,10 +11,8 @@ export const loginUser = async (loginParams) => {
 }
 
 export const registerUser = async (registerParams) => {
-  console.log(registerParams)
+  
   const resp = await api.post('/users', { user: registerParams })
-  console.log(registerParams)
-  console.log(resp)
   const token = resp.data.token
   const user = resp.data.user
   localStorage.setItem('authToken', token);
