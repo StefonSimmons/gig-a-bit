67cf5484c41b256514e5a3c4efbbb0baeba8eaf5
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-14 15:26:44 -0400

    Footer in place

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import {Divider} from './Header'
 
 const Foot = styled.footer`
-  position: absolute;
-  bottom: 0px;
   width: 100%;
-  height: .1vh;
   text-align: center;
-  opacity: .9
 `
 const Bottom = styled.div`
   display: flex;
   justify-content: space-around;
-  background-color: rgb(28,28,28);
-  padding: 1px 0; 
+  color: rgb(28,28,28);
+  padding: 25px 0; 
 `
 const Text = styled.h6`
   font-family: 'Merriweather Sans', sans-serif;
   font-size: 16px;
-  color: #DAE2DF;
+  color: rgb(28,28,28);
 `
 
 export default function Footer() {
   return (
     <div>
+      <Divider/>
       <Foot>
         <Bottom>
           <Text>About</Text>
