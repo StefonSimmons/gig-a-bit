852e6a10c76105ffd912e2a8de3c21399badb5f7
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-06-16 01:17:03 -0400

    edited comments in app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,38 +7,31 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, SECRET_KEY)
   end
 
-  #FOR USER Browser VERIFICATION
+  #For user Browser VERIFICATION
   def decode(header_token)
-    # Render/look at 'decoded_token' to see the 1st [0] and second [1] values
     # third value is the secret key.
-    # second value contains information about JWT algorithm that we use for encoding and decoding token . added at encode?
-    # first value is the payload
+    # second value contains information about JWT algorithm that JWT uses for encoding and decoding token
+    # first value is the payload. I'm grabbing this value
     decoded_token = JWT.decode(header_token, SECRET_KEY)[0]
-    # this just makes it so that the string token object returned from the split header can be idenitifed as a symbol when we .find it.
+    # this makes it so that the string token object returned from the split header can be idenitifed as a symbol when we .find it.
     HashWithIndifferentAccess.new decoded_token
   end
 
-  # FOR USER Browser VERIFICATION
+  # For user Browser VERIFICATION
   def authorize_request
     header = request.headers['Authorization']
     # Separates from the word 'Bearer'. on the front-end: token pulled from local storage droped into authorization again 
     header_token = header.split(' ').last 
     if header_token
-      # begin
+      begin
         @decoded = decode(header_token)
         ## grabs the user.id from the User database that matches the decoded token value
         @loggedin_user = User.find(@decoded[:user_id])
-      # rescue ActiveRecord::RecordNotFound => e
-      #   render json: { errors: e.message }, status: :unauthorized
-      # rescue JWT::DecodeError => e
-      #   render json: { errors: e.message }, status: :unauthorized
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+      end
     end
   end
 end
-
-
-# Whats' the difference between header and token and payload
-# token = new_user.id object (i.e. {user_id:new_user.id}) + secret_key
-  # -created when user registers/ is created. goes through a JWT.encode method to for encryption.
-# payload = token - secret_key. just an object
-# header = the 'Bearer'+encrypted_token encryption provided by the programmer to the front end. encrypted[payload.algo.secret_key]
